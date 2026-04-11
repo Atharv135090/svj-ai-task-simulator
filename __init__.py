859a2dc42a0e7815c27e78ab9aec7e12ef5087e6
Atharv Shinde <atharvshinde13062005@gmail.com>
2026-04-11 11:59:31 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .main import SVJEnvironment, Observation, Action, Reward
+from main import SVJEnvironment, Observation, Action, Reward
 
-__all__ = ["SVJEnvironment", "Observation", "Action", "Reward"]
+all= ["SVJEnvironment", "Observation", "Action", "Reward"]
